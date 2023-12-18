ac5fb996e47cfca346f4e0702d41af5a5a91eebb
Angkush <angkysahu2502@gmail.com>
2023-12-18 10:51:20 +0530

    [client]: added global loading bar

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+   return (
+      <main className="min-h-[80vh] flex items-center justify-center">
+         <section className="w-20 h-20 rounded-full border-8 border-primary border-t-muted animate-spin"></section>
+      </main>
+   );
+}
